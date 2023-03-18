12ff63207598878fc9b8910aca4472acf8b5969b
Tobias Krogh <tobias@krogh.de>
2023-03-18 15:19:49 +0100

    add e2e specs

--- e2e/src/e2e/app.cy.ts
+++ e2e/src/e2e/app.cy.ts
@@ -1,13 +1,56 @@
-import { getGreeting } from '../support/app.po';
+import {
+  generateButton,
+  getHeader,
+  noReportMessage,
+  reportOutput,
+  reportTableFirst,
+} from '../support/app.po';
 
 describe('test', () => {
-  beforeEach(() => cy.visit('/'));
+  beforeEach(() => {
+    cy.intercept('/mock-api/api/gateways', {
+      fixture: 'gateways.json',
+    }).as('Gateways');
+    cy.intercept('/mock-api/api/projects', {
+      fixture: 'projects.json',
+    }).as('Projects');
+    cy.intercept('/mock-api/api/users', {
+      fixture: 'users.json',
+    }).as('Users');
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
+    cy.intercept('/mock-api/api/report', {
+      fixture: 'report.json',
+    }).as('Report');
 
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome test');
+    cy.viewport(1440, 900);
+    cy.visit('/');
+  });
+
+  it('should behave properly', () => {
+    // internal redirect
+    cy.url().should('contain', '/reports');
+
+    cy.wait(['@Gateways', '@Projects', '@Users']);
+
+    // user data fetched and rendered
+    getHeader().contains('John Doe');
+
+    noReportMessage().should('exist');
+    reportOutput().should('not.exist');
+
+    // generate report
+    generateButton().click();
+
+    cy.wait(['@Report']);
+
+    noReportMessage().should('not.exist');
+    reportOutput().should('exist');
+
+    reportTableFirst().contains('15/04/2021');
+    reportTableFirst().contains('Gateway 2');
+    reportTableFirst().contains('6149cf56ae3f9cd9380f33ed');
+    reportTableFirst().contains('2,301.07 USD');
+
+    reportOutput().contains('TOTAL: 123,787.35 USD');
   });
 });
--- e2e/src/support/app.po.ts
+++ e2e/src/support/app.po.ts
@@ -1,1 +1,7 @@
-export const getGreeting = () => cy.get('h1');
+export const getHeader = () => cy.get('challenge-header');
+export const generateButton = () => cy.get('reports-filters [type="submit"]');
+export const noReportMessage = () => cy.get('reports-main .message-no-report');
+export const reportOutput = () => cy.get('reports-output');
+
+export const reportTableFirst = () =>
+  reportOutput().find('table tbody tr:first-child');
